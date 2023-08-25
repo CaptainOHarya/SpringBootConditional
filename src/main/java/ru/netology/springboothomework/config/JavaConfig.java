e5e086df26b47132e94a6a2afde064c479d6feb1
Leonid Zulin <leo.zulin@gmail.com>
2023-08-25 22:44:10 +0300

    Initial commit

---
+++ src/main/java/ru/netology/springboothomework/config/JavaConfig.java
@@ -0,0 +1,24 @@
+package ru.netology.springboothomework.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.springboothomework.profile.DevProfile;
+import ru.netology.springboothomework.profile.ProductionProfile;
+import ru.netology.springboothomework.profile.SystemProfile;
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
